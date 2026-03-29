75b9f2298cf634ae266825093dedc4906a4a5409
Geraldo <gmmc@simples.eti.br>
2026-03-29 22:58:21 +0000

    Salvando alterações

---
+++ n1_nc4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    int Idade;
+    int IDADE;
+    int idadE;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "sergio";
+    int _teste;
+    int teste_2;
+
+    char nome[20] = "Alice";  // Declara um array de 20 caracteres e inicializa com "Alice"
+
+    idade = 25;
+    quantidade = 10;
+}
