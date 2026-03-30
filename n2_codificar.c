665468e0cff4976bc29eaf31332b9299c3892f22
Geraldo <gmmc@simples.eti.br>
2026-03-30 16:51:30 +0000

    Salvando arquivo n2_codificar.c HORA DE CODificAR

---
+++ n2_codificar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+        int idade, matricula;
+        float altura;
+        char nome[50];
+
+        printf("Digite sua idade: ");
+        scanf("%d", &idade);
+
+        printf("Digite sua altura: ");
+        scanf("%f", &altura);
+
+        printf("Digite seu nome: ");
+        scanf("%s", nome);
+
+        printf("Digite sua matrícula: ");
+        scanf("%d", &matricula);
+
+        printf("\nNome do aluno: %s  ...  Matrícula: %d\n", nome, matricula);
+        printf("Idade: %d  ...  Altura: %.2f\n\n", idade, altura);
+
+        return 0;
+
+}
