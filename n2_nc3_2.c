708081d33228f97b296f9a6a470c532ccdd68468
Geraldo <gmmc@simples.eti.br>
2026-04-13 14:55:52 +0000

    // Estudando operadores de atribuição simples e compostos

---
+++ n2_nc3_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Atribuição simples (=)
+    Atribuição com soma (+=)
+    Atribuição com subtração (-=)
+    Atribuição com multiplicação (*=)
+    Atribuição com divisão (/=)
+    */
+
+    int numero1 = 10, numero2, resultado;
+
+    resultado = 10;
+    printf("Resultado: %d\n", resultado);
+
+    // resultado receberá o valor de (resultado + 20)
+    resultado += 20;
+    printf("Resultado: %d\n", resultado);
+
+    // resultado receberá o valor (resultado - valor de numero1)
+    resultado -= numero1;
+    printf("Resultado: %d\n", resultado);
+
+    resultado *= 5;
+    printf("Resultado: %d\n", resultado);
+
+    resultado /= 2;
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
